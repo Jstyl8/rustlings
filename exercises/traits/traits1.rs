9bee7c4d9be2ce2297da5a9c0dd171bbb623ec55
Jose Angel Garcia <joseangel.jstyl8@gmail.com>
2024-01-02 16:55:27 +0100

    feat(traits): done

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -7,14 +7,33 @@
 // Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(self) -> Self {
+        // - respecting signature, self without mut but cloning
+        // let mut bared = self.clone();
+        // bared.push_str("Bar");
+        // bared
+
+        /*
+        This code will work just as efficiently as if you had declared self
+        as mutable because self here is not a reference;
+        it's the actual owner of the string data.
+        By assigning self to new_string, we're just transferring the ownership,
+        not copying the data, which avoids the clone operation.
+        */
+        let mut new_string = self;
+        new_string.push_str("Bar");
+        new_string
+
+        // - is this allowed? I am changing the parameter to mut self
+        // self.push_str("Bar");
+        // self
+    }
 }
 
 fn main() {
